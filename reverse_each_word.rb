b8ba719ba55cf98110299f048165626249a73450
Juan Bermudez <juan.bermudez1102@outlook.com>
2019-09-23 02:35:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   sentence_to_array = sentence.split(" ")
-  reversed_sentence = ""
-  sentence_to_array.each{ |word| reversed_sentence += word.reverse+" " }
+  reversed_sentence = sentence_to_array.collect{ |word| word.reverse }
+  reversed_sentence.join(" ")
   reversed_sentence
 end
